9445b7c8b71e9bc0a8b321613d65ced47287bd95
Kaatje <kaatjenagels@gmail.com>
2026-09-11 09:23:21 +0200

    Update labs.config.js

--- js/labs.config.js
+++ js/labs.config.js
@@ -46,6 +46,15 @@ const LABS_CONFIG = [
     name: "Git Lab",
     tag: "General",
     path: "labs/git-lab.html",
+    ring: "ring-inner",
+    angle: 180,
+    glyph: '<path d="M8 2v4M8 10v4M2 8h4M10 8h4"/><circle cx="8" cy="8" r="2"/>'
+  },
+    {
+    id: "spelle",
+    name: "Style",
+    tag: "Programming",
+    path: "labs/spelle.html",
     ring: "ring-outer",
     angle: 180,
     glyph: '<path d="M8 2v4M8 10v4M2 8h4M10 8h4"/><circle cx="8" cy="8" r="2"/>'
